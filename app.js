e6cac32f05b1b4b386f24e62f8c8dbf1ed4bdc8f
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-07-29 16:40:43 +0530

    added mobile phones data and layouts files

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var hbs=require('express-handlebars')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'views/partials/'}))
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
