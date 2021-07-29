8678c196dd2d5fb49d793425def271f5631ad3ce
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-07-29 19:26:44 +0530

    last commit

--- app.js
+++ app.js
@@ -13,7 +13,15 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'views/partials/'}))
+app.engine('hbs', hbs({
+  extname: 'hbs', 
+  defaultLayout: 'layout', 
+  layoutsDir: path.join(__dirname, 'views/layout/'),
+  partialsDir  : [
+      //  path to your partials
+      path.join(__dirname, 'views/partials'),
+  ]
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
