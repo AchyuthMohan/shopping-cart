8f8592818f3ef979b9da9a5b8143f8eedf60a0df
Achyuth Mohan <achyuthmohan1@gmail.com>
2021-07-30 10:13:24 +0530

    created admin panel with all products

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
